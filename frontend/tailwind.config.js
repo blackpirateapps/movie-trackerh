09573fa396d3d19cf76d0dea59213bc294267141
blackpirateapps <github@blackpiratex.com>
2025-09-28 20:01:40 +0530

    init

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,16 @@
+import { join } from 'path'
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  // Use join from Node's path module to create absolute paths.
+  // This is more reliable in different build environments.
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    join(__dirname, 'index.html'),
+    join(__dirname, 'src/**/*.{js,ts,jsx,tsx}')
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
