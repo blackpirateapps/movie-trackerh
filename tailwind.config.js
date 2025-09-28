ec80b9abea607c2d5065235cdd96f1c51447b089
blackpirateapps <github@blackpiratex.com>
2025-09-28 21:27:09 +0530

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ export default {
         }
       },
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
+        sans: ['Inter Variable', 'Inter', 'system-ui', 'sans-serif'],
       },
       animation: {
         'fade-in': 'fadeIn 0.6s ease-out',
